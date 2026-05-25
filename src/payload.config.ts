71a93406e45de55e31bf71002e1c08c7598bfaa1
Ansif <stark858981@gmail.com>
2026-05-25 22:50:00 +0530

    integrated brevo email provider

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,4 +1,5 @@
 import { postgresAdapter } from '@payloadcms/db-postgres'
+import { nodemailerAdapter } from '@payloadcms/email-nodemailer'
 import {
   BoldFeature,
   EXPERIMENTAL_TableFeature,
@@ -78,7 +79,18 @@ export default buildConfig({
       ]
     },
   }),
-  //email: nodemailerAdapter(),
+  email: nodemailerAdapter({
+    defaultFromAddress: process.env.SMTP_FROM_ADDRESS || 'noreply@yourdomain.com',
+    defaultFromName: process.env.SMTP_FROM_NAME || 'Origrains',
+    transportOptions: {
+      host: process.env.SMTP_HOST || 'smtp-relay.brevo.com',
+      port: Number(process.env.SMTP_PORT) || 587,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    },
+  }),
   endpoints: [],
   globals: [Header, Footer],
   plugins,
